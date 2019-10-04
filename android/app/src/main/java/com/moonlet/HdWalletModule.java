2e75d423db98bfaf599da2b59a49eaa5b0dde6af
Burduhos George <george.burduhos@gmail.com>
2019-10-04 09:55:43 +0300

    native modules functions completed #4

--- android/app/src/main/java/com/moonlet/HdWalletModule.java
+++ android/app/src/main/java/com/moonlet/HdWalletModule.java
@@ -4,6 +4,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReadableArray;
 
 public class HdWalletModule extends ReactContextBaseJavaModule {
 
@@ -13,13 +14,23 @@ public class HdWalletModule extends ReactContextBaseJavaModule {
 
     @Override
     public String getName() {
-        return "HdWalletModule";
+        return "HdWallet";
     }
 
     @ReactMethod
-    public void generateWallet() {
-        
+    public void generateWallet(final Promise promise) {
+        promise.resolve("index");
     }
 
+    @ReactMethod
+    public void loadWallets(ReadableArray mnemonics,final Promise promise) {
+        promise.resolve("list of wallets");
+    }
 
+    @ReactMethod
+    public void getAccounts(int walletIndex, String blockchain, ReadableArray accountIndexes, final Promise promise) {
+        promise.resolve(walletIndex);
+    }
+
+   
 }

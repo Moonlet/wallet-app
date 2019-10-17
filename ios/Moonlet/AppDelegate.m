4cd9892c776a080f77a3433391cda86876cb30d0
Burduhos George <george.burduhos@gmail.com>
2019-10-17 09:32:53 +0300

    update appDelefate

--- ios/Moonlet/AppDelegate.m
+++ ios/Moonlet/AppDelegate.m
@@ -24,6 +24,9 @@
      jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
    #endif
 
+  
+  NSLog(@"jscodeLocation %@", jsCodeLocation.absoluteString);
+  
    RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation moduleName:@"Moonlet" initialProperties:nil launchOptions:launchOptions];
    rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 

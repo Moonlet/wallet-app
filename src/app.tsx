fd4229b2716e163b7fcefbb2dcb488dc77c48093
Burduhos George <george.burduhos@gmail.com>
2019-10-17 10:38:46 +0300

    update

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import { NativeModules } from 'react-native';
 import { Provider } from 'react-redux';
 import { createAppContainer } from 'react-navigation';
 import configureStore from './redux/config';
-import { RootNavigation } from './navigation';
+import { RootNavigation } from './navigation/navigation';
 import { darkTheme } from './core/theme/theme-dark';
 import { ThemeContext } from './core/theme/theme-contex';
 

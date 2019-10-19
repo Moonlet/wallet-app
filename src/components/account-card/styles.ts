1db6a9d52c3f0fe745f1c60d5fc6c16f71018963
Lucian Marica <maricalucian@gmail.com>
2019-10-20 01:14:52 +0300

    Temporary fix for android scrollview background bleeding

--- src/components/account-card/styles.ts
+++ src/components/account-card/styles.ts
@@ -1,11 +1,11 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
 import { ITheme } from '../../core/theme/itheme';
 
 export default (theme: ITheme) =>
     StyleSheet.create({
         container: {
             backgroundColor: theme.colors.cardBackground,
-            borderRadius: 6,
+            borderRadius: Platform.OS === 'ios' ? 6 : 0,
             height: 80,
             display: 'flex',
             justifyContent: 'space-between',

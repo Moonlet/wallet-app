ee596100b42c1fa82df848f1577d70d727a52640
Lucian Marica <maricalucian@gmail.com>
2019-10-22 09:56:36 +0300

    transparent blockchain selector bar, revisit colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,11 +1,11 @@
 export const COLORS = {
     WHITE: '#FFF',
     BLACK: '#000',
-    COD_GRAY: '#121212',
+    COD_GRAY: '#111111',
     SHARK_GRAY: '#232326',
-    DARK_GRAY: '#2f2f31',
-    GRAY: '#4E4E53',
-    LIGHT_GRAY: '#9A99A2',
+    DARK_GRAY: '#2d2d2f',
+    GRAY: '#58585d',
+    LIGHT_GRAY: '#9a99a2',
     HOT_GREEN: '#00E676',
     HOT_RED: '#E91E63',
     AQUA: '#00DAFF'

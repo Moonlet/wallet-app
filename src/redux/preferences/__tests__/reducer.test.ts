5c85e6d02d8b7c17c5758aeb1851c29f29713d1e
Burduhos George <george.burduhos@gmail.com>
2019-11-01 11:29:36 +0200

    Update reducer.test.ts

--- src/redux/preferences/__tests__/reducer.test.ts
+++ src/redux/preferences/__tests__/reducer.test.ts
@@ -2,6 +2,19 @@ import reducer from '../reducer';
 import * as actions from '../actions';
 
 describe('preferences reducer', () => {
+    test('should set initial state', () => {
+        const initialState = {
+            currency: 'USD',
+            pinLogin: true
+        };
+        expect(
+            reducer(initialState, {
+                type: '',
+                data: ''
+            })
+        ).toEqual({ currency: 'USD', pinLogin: true });
+    });
+
     test('should handle PREF_SET_PIN', () => {
         const initialState = {
             currency: 'USD',

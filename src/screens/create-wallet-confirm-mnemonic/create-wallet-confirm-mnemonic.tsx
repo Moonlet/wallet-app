1b336cf69a9d92b7e1dea5f66b2040dd71df704b
Tibi K <krisboi.tibi@gmail.com>
2019-11-05 14:41:39 +0200

    update pod file

--- src/screens/create-wallet-confirm-mnemonic/create-wallet-confirm-mnemonic.tsx
+++ src/screens/create-wallet-confirm-mnemonic/create-wallet-confirm-mnemonic.tsx
@@ -27,6 +27,10 @@ export interface IReduxProps {
     createHDWallet: (mnemonic: string, callback: () => any) => void;
 }
 
+const navigationOptions = ({ navigation }: any) => ({
+    title: 'Create'
+});
+
 /**
  * draws a textinput for a random mnemonic word
  * @param n {number}
@@ -142,10 +146,7 @@ export const CreateWalletConfirmMnemonicScreenComponent = (props: IProps & IRedu
         </View>
     );
 };
-
-export const navigationOptions = ({ navigation }: any) => ({
-    title: 'Create'
-});
+CreateWalletConfirmMnemonicScreenComponent.navigationOptions = navigationOptions;
 
 export const CreateWalletConfirmMnemonicScreen = smartConnect(
     CreateWalletConfirmMnemonicScreenComponent,
@@ -159,5 +160,3 @@ export const CreateWalletConfirmMnemonicScreen = smartConnect(
         withTheme(stylesProvider)
     ]
 );
-
-CreateWalletConfirmMnemonicScreen.navigationOptions = navigationOptions;

eb29c5780fcc253635cd0a12e72608cccde0f917
Burduhos George <george.burduhos@gmail.com>
2019-11-06 20:33:45 +0200

    update tests

---
+++ src/components/qr-modal/__tests__/qr-modal.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { QrModalReaderComponent, IProps } from '../qr-modal';
+import { loadTranslations } from '../../../core/i18n';
+
+import { shallow } from 'enzyme';
+
+const props: IProps = {
+    onQrCodeScanned: jest.fn()
+};
+
+describe('qr code modal ', () => {
+    beforeAll(async () => {
+        await loadTranslations('en');
+    });
+    test('renders correctly', () => {
+        const wrapper = shallow(<QrModalReaderComponent {...props} />);
+        expect(wrapper.debug()).toMatchSnapshot();
+    });
+});

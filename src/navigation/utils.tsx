8c6f19c6d1956c587f3e2e9df161aea258f1a0f3
Tibi K <krisboi.tibi@gmail.com>
2019-11-06 15:57:03 +0200

    configure sentry

--- src/navigation/utils.tsx
+++ src/navigation/utils.tsx
@@ -2,10 +2,26 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { Icon } from '../components/icon';
 import { COLORS } from '../styles/colors';
+import { Button } from '../library';
+import * as Sentry from '@sentry/react-native';
 
 export const DummyScreen = () => (
     <View style={{ display: 'flex', justifyContent: 'center', flex: 1 }}>
         <Text style={{ textAlign: 'center' }}>some screen</Text>
+        <Button
+            onPress={() => {
+                throw new Error('first error');
+            }}
+        >
+            JS Crash
+        </Button>
+        <Button
+            onPress={() => {
+                Sentry.nativeCrash();
+            }}
+        >
+            Native crash
+        </Button>
     </View>
 );
 

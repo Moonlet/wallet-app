6cbcedcd5b0fa88b004b4c18056af8f9a8604389
Lucian Marica <maricalucian@gmail.com>
2019-11-06 10:10:21 +0200

    Remove comments

--- src/core/secure/__tests__/encrypt.test.ts
+++ src/core/secure/__tests__/encrypt.test.ts
@@ -1,21 +1,6 @@
 import { hash, encrypt, decrypt } from '../encrypt';
 
-// jest.mock('../../../blockchain/blockchain-factory');
-// jest.mock('../mnemonic');
-// jest.mock('hdkey', () => {
-//     return {
-//         __esModule: true,
-//         default: () => ({
-//             derive: jest.fn()
-//         })
-//     };
-// });
-
 describe('encrypt', () => {
-    // Mnemonic.verify = jest.fn().mockReturnValue(true);
-    // Mnemonic.toSeed = jest.fn().mockReturnValue('MASTER_SEED');
-    // HDKey.fromMasterSeed = jest.fn();
-
     test('generate hash', async () => {
         expect(await hash('some password')).toBe('hashedkey');
     });

2933367096f002d5a480603d72bd861ee7e03250
maricalucian <maricalucian@gmail.com>
2019-11-11 22:43:55 +0200

    add recover wallet (#136)

--- src/redux/app/__tests__/actions.test.ts
+++ src/redux/app/__tests__/actions.test.ts
@@ -1,9 +1,8 @@
 import { appSwitchWallet, appSetTosVersion } from '../actions';
 
-
 describe('app actions', () => {
     test('app action creators', () => {
-        expect(appSwitchWallet(1)).toMatchSnapshot();    
-        expect(appSetTosVersion(1)).toMatchSnapshot();    
+        expect(appSwitchWallet(1)).toMatchSnapshot();
+        expect(appSetTosVersion(1)).toMatchSnapshot();
     });
 });

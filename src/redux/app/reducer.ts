26b827f59dfdf3b111b7cf7ebe4ccdabd0a5ee32
Tibi K <krisboi.tibi@gmail.com>
2019-11-13 16:05:28 +0200

    add support for current network

--- src/redux/app/reducer.ts
+++ src/redux/app/reducer.ts
@@ -1,10 +1,23 @@
 import { IAppState } from './state';
 import { IAction } from '../types';
 import { APP_SWITCH_WALLET, APP_SET_TOS_VERSION } from './actions';
+import { Blockchain } from '../../core/blockchain/types';
 
 const intialState: IAppState = {
     currentWalletIndex: 1,
-    tosVersion: 0
+    tosVersion: 0,
+    devMode: true,
+    testNet: true,
+    networks: {
+        [Blockchain.ETHEREUM]: {
+            testNet: 4,
+            mainNet: 1
+        },
+        [Blockchain.ZILLIQA]: {
+            testNet: 333,
+            mainNet: 1
+        }
+    }
 };
 
 export default (state: IAppState = intialState, action: IAction): IAppState => {
--- src/redux/app/state.ts
+++ src/redux/app/state.ts
@@ -1,4 +1,15 @@
+export interface INetworksOptions {
+    [blockchain: string]: {
+        // key is for Blockchain enum
+        mainNet?: number;
+        testNet?: number;
+    };
+}
+
 export interface IAppState {
     currentWalletIndex: number;
     tosVersion: number;
+    devMode: boolean;
+    testNet: boolean;
+    networks: INetworksOptions;
 }

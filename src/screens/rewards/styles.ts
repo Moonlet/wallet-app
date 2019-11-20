95fb571d2cf3fe08368cc17c4aafb385e6bd1a91
Andrei Novac <32679726+NovacAndrei@users.noreply.github.com>
2019-11-20 16:05:11 +0200

    Rewards screen UI (#169)
    * Rewards screen, Skeleton component
    
    * fix PR

---
+++ src/screens/rewards/styles.ts
@@ -0,0 +1,41 @@
+import { StyleSheet } from 'react-native';
+import { ITheme } from '../../core/theme/itheme';
+import { BASE_DIMENSION } from '../../styles/dimensions';
+import { pw, ph } from '../../styles';
+
+export default (theme: ITheme) =>
+    StyleSheet.create({
+        container: {
+            paddingHorizontal: BASE_DIMENSION * 2,
+            paddingTop: 40,
+            justifyContent: 'center',
+            backgroundColor: theme.colors.appBackground,
+            ...StyleSheet.absoluteFillObject
+        },
+        logoImage: {
+            height: ph(20),
+            width: pw(40),
+            alignSelf: 'center',
+            resizeMode: 'contain'
+        },
+        launchingSoonText: {
+            fontWeight: 'bold',
+            fontSize: 22,
+            lineHeight: 28,
+            textAlign: 'center',
+            letterSpacing: 0.35,
+            color: theme.colors.text,
+            opacity: 0.87,
+            marginBottom: BASE_DIMENSION
+        },
+        newSectionText: {
+            fontSize: 17,
+            lineHeight: 22,
+            textAlign: 'center',
+            color: theme.colors.textSecondary,
+            marginBottom: BASE_DIMENSION * 7
+        },
+        skeletonRow: {
+            marginBottom: BASE_DIMENSION * 2
+        }
+    });

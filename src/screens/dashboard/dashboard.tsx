332b29d88f8850ec64be4a6bc5f3cbf51ec34b62
Andrei Novac <andrei.novac.96@gmail.com>
2019-11-20 17:13:36 +0200

    Coins ScrollView

--- src/screens/dashboard/dashboard.tsx
+++ src/screens/dashboard/dashboard.tsx
@@ -210,33 +210,58 @@ export class DashboardScreenComponent extends React.Component<IProps & IReduxPro
                     />
                 </Animated.View>
 
-                <LinearGradient
-                    colors={this.props.theme.shadowGradient}
-                    locations={[0, 0.5]}
-                    style={styles.selectorGradientContainer}
-                >
-                    <View style={styles.blockchainSelectorContainer} testID="blockchainSelector">
-                        {this.state.coins.map((coin, i) => (
-                            <TouchableOpacity
-                                key={i}
-                                style={[
-                                    styles.blockchainButton,
-                                    this.state.coinIndex === i && styles.blockchainButtonActive
-                                ]}
-                                onPress={() => this.setActiveCoin(i)}
+                {this.state.coins && this.state.coins.length > 1 ? (
+                    <LinearGradient
+                        colors={this.props.theme.shadowGradient}
+                        locations={[0, 0.5]}
+                        style={styles.selectorGradientContainer}
+                    >
+                        <View
+                            style={styles.blockchainSelectorContainer}
+                            testID="blockchainSelector"
+                        >
+                            <ScrollView
+                                horizontal
+                                disableIntervalMomentum={true}
+                                overScrollMode={'never'}
+                                centerContent={true}
+                                snapToAlignment={'start'}
+                                contentContainerStyle={{ flexGrow: 1 }}
+                                showsHorizontalScrollIndicator={false}
+                                snapToStart={false}
+                                snapToEnd={false}
+                                decelerationRate={0.8}
                             >
-                                <Text
-                                    style={
-                                        this.state.coinIndex === i &&
-                                        styles.blockchainButtonTextActive
-                                    }
-                                >
-                                    {BLOCKCHAIN_INFO[this.state.coins[i]].coin}
-                                </Text>
-                            </TouchableOpacity>
-                        ))}
-                    </View>
-                </LinearGradient>
+                                {this.state.coins.map((coin, i) => (
+                                    <TouchableOpacity
+                                        key={i}
+                                        style={[
+                                            styles.blockchainButton,
+                                            this.state.coinIndex === i &&
+                                                styles.blockchainButtonActive,
+                                            {
+                                                width:
+                                                    this.state.coins.length > 3
+                                                        ? SCREEN_WIDTH / 3
+                                                        : null
+                                            }
+                                        ]}
+                                        onPress={() => this.setActiveCoin(i)}
+                                    >
+                                        <Text
+                                            style={
+                                                this.state.coinIndex === i &&
+                                                styles.blockchainButtonTextActive
+                                            }
+                                        >
+                                            {BLOCKCHAIN_INFO[this.state.coins[i]].coin}
+                                        </Text>
+                                    </TouchableOpacity>
+                                ))}
+                            </ScrollView>
+                        </View>
+                    </LinearGradient>
+                ) : null}
             </View>
         );
     }

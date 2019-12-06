c483a2ad15200bd56c16ada01f522e01bbd57663
maricalucian <maricalucian@gmail.com>
2019-12-06 23:04:06 +0200

    notification support and silent notifications for android only (#216)

--- ios/Moonlet/AppDelegate.m
+++ ios/Moonlet/AppDelegate.m
@@ -11,6 +11,10 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <Firebase.h>
+#import "RNFirebaseNotifications.h"
+#import "RNFirebaseMessaging.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -34,6 +38,8 @@
    self.window.rootViewController = rootViewController;
    [self.window makeKeyAndVisible];
   
+  [FIRApp configure];
+  [RNFirebaseNotifications configure];
   
   return YES;
 }
@@ -45,6 +51,20 @@
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
+  [[RNFirebaseNotifications instance] didReceiveLocalNotification:notification];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(nonnull NSDictionary *)userInfo
+fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler{
+  [[RNFirebaseNotifications instance] didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+}
+
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings {
+  [[RNFirebaseMessaging instance] didRegisterUserNotificationSettings:notificationSettings];
 }
 
 @end
---
+++ src/messaging/notifications.web.ts
@@ -0,0 +1,11 @@
+export class NotificationService {
+    public async getToken() {
+        return 'x';
+    }
+
+    public configure() {
+        return null;
+    }
+}
+
+export const Notification = new NotificationService();

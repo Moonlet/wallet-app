333a0991774fc0a2e7c261cb55250a3f9f087c35
Tibi K <krisboi.tibi@gmail.com>
2020-01-15 13:55:26 +0200

    update script

--- scripts/ts-nockeck-files.sh
+++ scripts/ts-nockeck-files.sh
@@ -1,13 +1,13 @@
+#!/bin/bash
+
 FILES=(
     "node_modules/react-native-draggable-flatlist/index.tsx"
     "node_modules/react-native-draggable-flatlist/procs.tsx"
 )
 
 echo "Patching files...";
-for file in ${FILES[@]}; do
+for file in "${FILES[@]}"; do
     echo "Adding //ts-nocheck to $file..."
-    grep -q "//@ts-nocheck" $file || printf "//@ts-nocheck\n\n$(cat $file)" > $file
+    grep -q "//@ts-nocheck" "$file" || printf "//@ts-nocheck\n\n%s" "$(cat "$file")" > "$file"
 done
 echo "Done."
-
-# 

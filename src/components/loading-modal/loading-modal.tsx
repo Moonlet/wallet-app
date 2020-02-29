7e843a5f65fc0671c8d8a11b3ee0201cc0f8b30c
Andrei Novac <andrei.novac.96@gmail.com>
2020-02-29 13:41:24 +0200

    modal rn web

--- src/components/loading-modal/loading-modal.tsx
+++ src/components/loading-modal/loading-modal.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, ActivityIndicator, Modal, Platform } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 import { withTheme, IThemeProps } from '../../core/theme/with-theme';
 import stylesProvider from './styles';
 import { smartConnect } from '../../core/utils/smart-connect';
 import { IReduxState } from '../../redux/state';
 import { connect } from 'react-redux';
+import Modal from '../../core/utils/modal';
 
 interface IExternalProps {
     spinnerColor?: string;
@@ -23,21 +24,6 @@ const mapStateToProps = (state: IReduxState) => {
 export const LoadingModalComponent = (
     props: IReduxProps & IExternalProps & IThemeProps<ReturnType<typeof stylesProvider>>
 ) => {
-    if (Platform.OS === 'web') {
-        if (props.isVisible) {
-            return (
-                <View style={props.styles.webContainer}>
-                    <ActivityIndicator
-                        size="large"
-                        color={props.spinnerColor ? props.spinnerColor : props.theme.colors.accent}
-                    />
-                </View>
-            );
-        } else {
-            return <View />;
-        }
-    }
-
     return (
         <Modal
             animationType="fade"
--- src/components/loading-modal/styles.ts
+++ src/components/loading-modal/styles.ts
@@ -7,14 +7,5 @@ export default (theme: ITheme) =>
             flex: 1,
             backgroundColor: theme.colors.overlayBackground,
             justifyContent: 'center'
-        },
-        webContainer: {
-            position: 'absolute',
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            backgroundColor: theme.colors.overlayBackground,
-            justifyContent: 'center'
         }
     });

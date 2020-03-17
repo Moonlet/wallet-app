91fcd8e912c1e4edfad18fbbc24240df684a3bad
Burduhos George <george.burduhos@gmail.com>
2020-03-17 09:58:36 +0200

    Delete - ios keychain on reinstall (#475)
    * Update keychain.ts
    
    * Update keychain.test.ts
    
    Co-authored-by: Tibi Krisboi <krisboi.tibi@gmail.com>

--- src/core/secure/__tests__/keychain.test.ts
+++ src/core/secure/__tests__/keychain.test.ts
@@ -1,5 +1,19 @@
 import { setPassword, getPassword } from '../keychain';
 
+jest.mock('@react-native-community/async-storage', () => ({
+    setItem: jest.fn(() => {
+        return new Promise((resolve, reject) => {
+            resolve(null);
+        });
+    }),
+
+    getItem: jest.fn(() => {
+        return new Promise((resolve, reject) => {
+            resolve(JSON.stringify(''));
+        });
+    })
+}));
+
 describe('keychain', () => {
     test('set password', async () => {
         await setPassword('some password');

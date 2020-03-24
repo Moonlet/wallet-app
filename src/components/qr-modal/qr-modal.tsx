e3b003c46f2f25bccd429f9e3f0980886c500ec3
Andrei Novac <andrei.novac.96@gmail.com>
2020-03-24 12:25:14 +0200

    request permission

--- src/components/qr-modal/qr-modal.tsx
+++ src/components/qr-modal/qr-modal.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { View, Modal, Platform, Linking, PermissionsAndroid } from 'react-native';
-import { CameraKitCameraScreen, CameraKitCamera } from 'react-native-camera-kit';
+import { View, Modal, Platform, Linking } from 'react-native';
+import { CameraKitCameraScreen } from 'react-native-camera-kit';
 import { translate } from '../../core/i18n';
 import AndroidOpenSettings from 'react-native-android-open-settings';
 import { Dialog } from '../dialog/dialog';
 import { smartConnect } from '../../core/utils/smart-connect';
 import { setDisplayPasswordModal } from '../../redux/ui/password-modal/actions';
 import { connect } from 'react-redux';
+import { checkDeviceCameraPermission } from '../../core/utils/request-permissions';
 
 export interface IExternalProps {
     onQrCodeScanned: (qrCode: string) => any;
@@ -49,37 +50,12 @@ export class QrModalReaderComponent extends React.Component<IExternalProps & IRe
         }
     }
 
-    private async requestCameraPermission() {
-        try {
-            const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA);
-            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-                return true;
-            } else {
-                return null;
-            }
-        } catch (err) {
-            throw null;
-        }
-    }
-
     public open = async () => {
         this.props.setDisplayPasswordModal(false);
 
-        let success = await CameraKitCamera.checkDeviceCameraAuthorizationStatus();
+        const accessGranted = await checkDeviceCameraPermission();
 
-        if (success === -1) {
-            if (Platform.OS === 'android') {
-                try {
-                    success = await this.requestCameraPermission();
-                } catch (err) {
-                    success = null;
-                }
-            } else {
-                success = await CameraKitCamera.requestDeviceCameraAuthorization();
-            }
-        }
-
-        if (success) {
+        if (accessGranted) {
             this.setState({ isVisible: true });
         } else {
             Dialog.alert(
@@ -95,8 +71,6 @@ export class QrModalReaderComponent extends React.Component<IExternalProps & IRe
                 }
             );
         }
-
-        this.props.setDisplayPasswordModal(true);
     };
 
     public render() {
@@ -111,11 +85,15 @@ export class QrModalReaderComponent extends React.Component<IExternalProps & IRe
                     <CameraKitCameraScreen
                         testID="CameraKitCameraScreen"
                         actions={{ leftButtonText: translate('App.labels.cancel') }}
-                        onBottomButtonPressed={() => this.setState({ isVisible: false })}
+                        onBottomButtonPressed={() => {
+                            this.setState({ isVisible: false });
+                            this.props.setDisplayPasswordModal(true);
+                        }}
                         scanBarcode={true}
                         onReadCode={event => {
                             this.setState({ isVisible: false });
                             this.props.onQrCodeScanned(event.nativeEvent.codeStringValue);
+                            this.props.setDisplayPasswordModal(true);
                         }}
                     />
                 </View>

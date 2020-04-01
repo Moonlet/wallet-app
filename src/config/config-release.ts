b17878c249a284ab43efd94303571655d2c7a174
Andrei Novac <andrei.novac.96@gmail.com>
2020-04-01 11:29:06 +0300

    set firebase fetch duration

--- src/config/config-release.ts
+++ src/config/config-release.ts
@@ -3,7 +3,8 @@ import { IAppConfig } from './config-interface';
 export const CONFIG: IAppConfig = {
     supportUrl: 'https://moonlet.xyz/links/support',
     env: process.env.MOONLET_SOME_KEY,
-    tokensUrl: 'https://static.moonlet.xyz/tokens/'
+    tokensUrl: 'https://static.moonlet.xyz/tokens/',
+    firebaseFetchDuration: 15 * 60 // 15 mins
 };
 
 export default CONFIG;

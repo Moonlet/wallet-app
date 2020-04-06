567fff1d3f0bdb963412262cb1e91b5937b229ef
Andrei Novac <andrei.novac.96@gmail.com>
2020-04-06 23:56:12 +0300

    revert app.ts

--- src/core/constants/app.ts
+++ src/core/constants/app.ts
@@ -9,8 +9,8 @@ export const WC = {
 export const DISPLAY_HINTS_TIMES = 3;
 export const RESET_APP_FAILED_LOGINS = 15;
 export const FAILED_LOGIN_BLOCKING = {
-    3: 1 * 30 * 1000, // 5 min
-    6: 1 * 35 * 1000, // 20 min
-    9: 1 * 40 * 1000, // 1h
+    3: 5 * 60 * 1000, // 5 min
+    6: 20 * 60 * 1000, // 20 min
+    9: 60 * 60 * 1000, // 1h
     12: 24 * 60 * 60 * 1000 // 1 day
 };

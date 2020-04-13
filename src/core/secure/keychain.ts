95d9f6347bc8c40c884b564234bce645c4f7fb7a
georgeburduhos <george.burduhos@gmail.com>
2020-04-13 21:57:47 +0300

    Update keychain.ts

--- src/core/secure/keychain.ts
+++ src/core/secure/keychain.ts
@@ -1,9 +1,8 @@
 import * as Keychain from 'react-native-keychain';
 import { generateRandomEncryptionKey, hash } from './encrypt';
 import { storeEncrypted, readEncrypted, deleteFromStorage } from './storage';
-import DeviceInfo from 'react-native-device-info';
 import uuidv4 from 'uuid/v4';
-import { Settings } from 'react-native';
+import { Platform } from 'react-native';
 
 const defaultOptions = {
     serviceEncryption: 'com.moonlet.encryption',
@@ -15,13 +14,16 @@ const defaultOptions = {
 export const KEY_PIN_SAMPLE = 'moonletPinSample';
 
 export const clearKeychain = async () => {
-    if (!Settings.get('appIsInstalled')) {
-        clearPinCode();
-        deleteFromStorage(KEY_PIN_SAMPLE);
-        await Keychain.resetGenericPassword({ service: defaultOptions.serviceEncryption });
-        Settings.set({
-            appIsInstalled: true
-        });
+    if (Platform.OS === 'ios') {
+        const Settings = require('react-native').Settings;
+        if (!Settings.get('appIsInstalled')) {
+            clearPinCode();
+            deleteFromStorage(KEY_PIN_SAMPLE);
+            await Keychain.resetGenericPassword({ service: defaultOptions.serviceEncryption });
+            Settings.set({
+                appIsInstalled: true
+            });
+        }
     }
 };
 
@@ -116,9 +118,6 @@ export const getPinCode = async () => {
     } catch (error) {
         // console.log('keychain error', JSON.stringify(error, null, 4));
         if (error.message.indexOf('Authentication failed') >= 0) {
-            if (DeviceInfo.getManufacturerSync() === 'OnePlus') {
-                return Promise.reject('FAILED');
-            }
             return getPinCode();
         } else if (
             error.message.indexOf('Cancel') >= 0 ||

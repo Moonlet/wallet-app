20289add7a3426ef051b1d4358b45c3a0e7d3c1c
Tibi Krisboi <krisboi.tibi@gmail.com>
2020-04-21 12:07:08 +0300

    Prod setup  (#677)
    * production setup
    
    * terms urls with cache invalidate

--- src/config/config-release.ts
+++ src/config/config-release.ts
@@ -5,8 +5,9 @@ export const CONFIG: IAppConfig = {
     env: process.env.MOONLET_SOME_KEY,
     tokensUrl: 'https://fire.moonlet.io/static/tokens/',
     dataApiUrl: 'https://api.moonlet.io/data',
-    termsAndConditionsUrl: 'https://fire.moonlet.io/static/terms/terms-of-service.html',
-    privacyPolicyUrl: 'https://fire.moonlet.io/static/terms/privacy-policy.html',
+    termsAndConditionsUrl:
+        'https://fire.moonlet.io/static/terms/terms-of-service.html?v=' + Date.now(),
+    privacyPolicyUrl: 'https://fire.moonlet.io/static/terms/privacy-policy.html?v=' + Date.now(),
     firebaseConfigFetchInterval: 15 * 60, // 15 mins
     ntpServer: 'pool.ntp.org',
     ntpPort: 123

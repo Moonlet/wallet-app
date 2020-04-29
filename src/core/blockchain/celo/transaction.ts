61f7947e7d6eec5305c12b1137262ee624a2752f
Tibi K <krisboi.tibi@gmail.com>
2020-04-30 01:31:09 +0300

    add signing support for celo

--- src/core/blockchain/celo/transaction.ts
+++ src/core/blockchain/celo/transaction.ts
@@ -5,28 +5,57 @@ import { Celo } from '.';
 import { TokenType } from '../types/token';
 import { TransactionStatus } from '../../wallet/types';
 import abi from 'ethereumjs-abi';
-import { Transaction } from 'ethereumjs-tx';
 import BigNumber from 'bignumber.js';
+
+import elliptic from 'elliptic';
+import { Hash, RLP } from 'eth-lib';
+
+const toHex = value => {
+    if (value && value !== '0x') {
+        const base = typeof value === 'string' && value.indexOf('0x') === 0 ? 16 : 10;
+
+        let stringValue = new BigNumber(value, base).toString(16);
+        if (stringValue.length % 2 > 0) {
+            stringValue = '0' + stringValue;
+        }
+        return '0x' + stringValue;
+    }
+    return '0x';
+};
 
 export class CeloTransactionUtils extends EthereumTransactionUtils {
     public sign = async (tx: IBlockchainTransaction, privateKey: string): Promise<any> => {
-        const transaction = new Transaction(
-            {
-                nonce: '0x' + tx.nonce.toString(16),
-                gasPrice: '0x' + new BigNumber(tx.feeOptions.gasPrice).toString(16),
-                gasLimit: '0x' + new BigNumber(tx.feeOptions.gasLimit).toString(16),
-                to: tx.toAddress,
-                value: '0x' + new BigNumber(tx.amount).toString(16),
-                data: tx.data?.raw
-            },
-            {
-                chain: tx.chainId
-            }
-        );
+        const txData = [
+            toHex(tx.nonce),
+            toHex(tx.feeOptions.gasPrice),
+            toHex(tx.feeOptions.gasLimit),
+            '0x', // feeCurrency
+            '0x', // gatewayFeeRecipient
+            '0x', // gatewayFee
+            (tx.toAddress || '0x').toLowerCase(),
+            toHex(tx.amount),
+            (tx.data.raw || '0x').toLowerCase(),
+            toHex(tx.chainId || 1)
+        ];
+        const encodedTx = RLP.encode(txData.concat(['0x', '0x']));
+        const txHash = Hash.keccak256(encodedTx);
+        const addToV = Number(tx.chainId) * 2 + 35;
 
-        transaction.sign(Buffer.from(privateKey, 'hex'));
+        privateKey = '0x' + privateKey.replace(/^0x/gi, '').toLowerCase();
 
-        return '0x' + transaction.serialize().toString('hex');
+        const ecSignature = new elliptic.ec('secp256k1')
+            .keyFromPrivate(Buffer.from(privateKey.replace(/^0x/gi, ''), 'hex'))
+            .sign(Buffer.from(txHash.replace(/^0x/gi, ''), 'hex'), { canonical: true });
+
+        const signature = {
+            v: toHex(addToV + ecSignature.recoveryParam),
+            r: '0x' + ecSignature.r.toString(16),
+            s: '0x' + ecSignature.s.toString(16)
+        };
+
+        const rawTx = txData.slice(0, 9).concat([signature.v, signature.r, signature.s]);
+
+        return RLP.encode(rawTx);
     };
 
     public buildTransferTransaction = async (

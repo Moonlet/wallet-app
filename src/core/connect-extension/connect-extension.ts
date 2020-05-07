0cb3c7c6b4121ec8237376850e958b8a15a87755
Andrei Novac <andrei.novac.96@gmail.com>
2020-05-07 21:05:05 +0300

    remove

--- src/core/connect-extension/connect-extension.ts
+++ src/core/connect-extension/connect-extension.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '../utils/http-client';
 import CONFIG from '../../config/config-beta';
-import { encrypt } from '../secure/encrypt.web'; // TODO: check this, not oke .web
+import { encrypt } from '../secure/encrypt.web';
 import { extensionState } from './conn-ext-trim-state';
 import { store } from '../../redux/config';
 import { Notifications } from '../messaging/notifications/notifications';

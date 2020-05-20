1fc85ec8ebfe5d6999361de61519dce470754489
Tibi Krisboi <krisboi.tibi@gmail.com>
2020-05-20 11:19:24 +0300

    Fix sentry errors (#749)
    * fix WALLET-APP-ANDROID-1F
    
    * fix zilliqa tx update
    
    * improve extension sync

--- src/core/secure/encrypt/encrypt.ts
+++ src/core/secure/encrypt/encrypt.ts
@@ -1,14 +1,36 @@
 import { NativeModules } from 'react-native';
-import { SALT, HASH_COST, HASH_LENGTH, SEPARATOR, IV_LENGTH } from './consts';
-
+import { SALT, HASH_COST, HASH_LENGTH, SEPARATOR, ITERATIONS, IV_LENGTH } from './consts';
+import { captureException } from '@sentry/react-native';
+import pbkdf2 from 'crypto-js/pbkdf2';
+import { v4 as uuidv4 } from 'uuid';
+import CryptoJS from 'crypto-js';
 const Aes = NativeModules.Aes;
 
 export const hash = async (text: string, salt: string = SALT): Promise<string> =>
     Aes.pbkdf2(text, salt, HASH_COST, HASH_LENGTH);
 
 export const generateRandomEncryptionKey = async (): Promise<string> => {
-    const randomKey = await Aes.randomKey(IV_LENGTH);
-    return Aes.pbkdf2(randomKey, SALT, HASH_COST, HASH_LENGTH);
+    try {
+        const randomKey = await Aes.randomKey(IV_LENGTH);
+        const encryptionKey = await Aes.pbkdf2(randomKey, SALT, HASH_COST, HASH_LENGTH);
+
+        if (encryptionKey) {
+            return encryptionKey;
+        } else {
+            throw new Error(
+                'generateRandomEncryptionKey: generated encryption key evaluates to falsy. (fallback to js implementation to avoid crash)'
+            );
+        }
+    } catch (err) {
+        captureException(err);
+
+        return pbkdf2(
+            uuidv4(),
+            SALT,
+            { keySize: 512 / 32, iterations: ITERATIONS },
+            HASH_LENGTH
+        ).toString(CryptoJS.enc.Base64);
+    }
 };
 
 // aes encrypt

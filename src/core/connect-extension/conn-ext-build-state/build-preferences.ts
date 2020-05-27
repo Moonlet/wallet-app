a145ca382a8c0a3d920ab6e590ed4cfb5b0868dc
Andrei Novac <andrei.novac.96@gmail.com>
2020-05-27 16:20:39 +0300

    trim pref blockchains

--- src/core/connect-extension/conn-ext-build-state/build-preferences.ts
+++ src/core/connect-extension/conn-ext-build-state/build-preferences.ts
@@ -1,32 +1,11 @@
 import * as IExtStorage from '../types';
 import cloneDeep from 'lodash/cloneDeep';
-import { BLOCKCHAIN_LIST } from '../../blockchain/blockchain-factory';
 
 export const buildPreferences = (trimmedPreferences: IExtStorage.IStoragePreferences) => {
-    const blockchains = {};
-
-    trimmedPreferences.blockchains.map((blockchain: string, index: number) => {
-        Object.assign(blockchains, {
-            ...blockchains,
-            [blockchain]: {
-                order: index,
-                active: true
-            }
-        });
-    });
-
-    // add inactive blockchains
-    BLOCKCHAIN_LIST.filter(b => !trimmedPreferences.blockchains.includes(b)).map(blockchain => {
-        blockchains[blockchain] = {
-            order: 999,
-            active: false
-        };
-    });
-
     return {
         currency: trimmedPreferences.currency,
         testNet: trimmedPreferences.testNet,
         networks: cloneDeep(trimmedPreferences.networks),
-        blockchains
+        blockchains: cloneDeep(trimmedPreferences.blockchains)
     };
 };
--- src/core/connect-extension/conn-ext-trim-state.ts
+++ src/core/connect-extension/conn-ext-trim-state.ts
@@ -2,10 +2,11 @@ import { IWalletsState, IWalletState, IAccountState } from '../../redux/wallets/
 import { IReduxState } from '../../redux/state';
 import cloneDeep from 'lodash/cloneDeep';
 import * as IExtStorage from './types';
-import { IPrefState, IBlockchainsOptions } from '../../redux/preferences/state';
 import { IContactsState } from '../../redux/contacts/state';
 import { ITokensConfigState, ITokenConfigState } from '../../redux/tokens/state';
 import { ChainIdType, IBlockchainTransaction, Blockchain } from '../blockchain/types';
+import { getBlockchains } from '../../redux/preferences/selectors';
+import { BLOCKCHAIN_LIST } from '../blockchain/blockchain-factory';
 
 export const trimWallets = (wallets: IWalletsState) => {
     const trimmedWallets: IExtStorage.IStorageWallets = {};
@@ -101,18 +102,37 @@ export const trimTokens = (tokens: ITokensConfigState): IExtStorage.IStorageToke
     return trimmedTokens;
 };
 
-const trimPrefBlockchains = (blockchains: IBlockchainsOptions) => {
-    return Object.keys(blockchains)
-        .filter((blockchain: Blockchain) => blockchains[blockchain].active === true)
-        .sort((a, b) => blockchains[a].order - blockchains[b].order);
+const trimPrefBlockchains = (state: IReduxState) => {
+    const activeBlockchains: Blockchain[] = getBlockchains(state);
+    const trimBlockchains = {};
+
+    activeBlockchains.map((blockchain: string, index: number) => {
+        Object.assign(trimBlockchains, {
+            ...trimBlockchains,
+            [blockchain]: {
+                order: index,
+                active: true
+            }
+        });
+    });
+
+    // Add inactive Blockchains
+    BLOCKCHAIN_LIST.filter(b => !activeBlockchains.includes(b)).map(blockchain => {
+        trimBlockchains[blockchain] = {
+            order: 999,
+            active: false
+        };
+    });
+
+    return trimBlockchains;
 };
 
-export const trimPreferences = (preferences: IPrefState): IExtStorage.IStoragePreferences => {
+export const trimPreferences = (state: IReduxState): IExtStorage.IStoragePreferences => {
     return {
-        currency: preferences.currency,
-        testNet: preferences.testNet,
-        networks: cloneDeep(preferences.networks),
-        blockchains: trimPrefBlockchains(preferences.blockchains)
+        currency: state.preferences.currency,
+        testNet: state.preferences.testNet,
+        networks: cloneDeep(state.preferences.networks),
+        blockchains: trimPrefBlockchains(state)
     };
 };
 
@@ -123,7 +143,7 @@ export const trimContacts = (contacts: IContactsState): IExtStorage.IStorageCont
 const trimState = (state: IReduxState) => ({
     wallets: trimWallets(state.wallets),
     contacts: trimContacts(state.contacts),
-    preferences: trimPreferences(state.preferences),
+    preferences: trimPreferences(state),
     tokens: trimTokens(state.tokens)
 });
 

072cf9c91e0e2ccd119355936011f393dee52cba
Andrei Novac <andrei.novac.96@gmail.com>
2020-05-28 10:45:53 +0300

    update

--- src/core/connect-extension/conn-ext-build-state/build-preferences.ts
+++ src/core/connect-extension/conn-ext-build-state/build-preferences.ts
@@ -1,11 +1,40 @@
-import * as IExtStorage from '../types';
 import cloneDeep from 'lodash/cloneDeep';
+import { BLOCKCHAIN_LIST } from '../../blockchain/blockchain-factory';
 
-export const buildPreferences = (trimmedPreferences: IExtStorage.IStoragePreferences) => {
+export const buildPreferences = (trimmedPreferences: any) => {
+    let blockchains;
+
+    if (Array.isArray(trimmedPreferences.blockchains)) {
+        // Array of active Blockchains - older versions
+        trimmedPreferences.blockchains.map((blockchain: string, index: number) => {
+            Object.assign(blockchains, {
+                ...blockchains,
+                [blockchain]: {
+                    order: index,
+                    active: true
+                }
+            });
+        });
+
+        // add inactive blockchains
+        trimmedPreferences.blockchains.length > 0 &&
+            BLOCKCHAIN_LIST.filter(b => !trimmedPreferences.blockchains.includes(b)).map(
+                blockchain => {
+                    blockchains[blockchain] = {
+                        order: 999,
+                        active: false
+                    };
+                }
+            );
+    } else {
+        // IBlockchainsOptions type
+        blockchains = cloneDeep(trimmedPreferences.blockchains);
+    }
+
     return {
         currency: trimmedPreferences.currency,
         testNet: trimmedPreferences.testNet,
         networks: cloneDeep(trimmedPreferences.networks),
-        blockchains: cloneDeep(trimmedPreferences.blockchains)
+        blockchains
     };
 };

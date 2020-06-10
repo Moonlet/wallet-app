21a23a354243b5c24f637db6bd56139351fb9d7b
Andrei Novac <andrei.novac.96@gmail.com>
2020-06-10 18:59:37 +0300

    refactor send common/utils

--- e2e/tests/send/common.ts
+++ e2e/tests/send/common.ts
@@ -1,5 +1,4 @@
 import * as utils from '../../utils/utils';
-import * as customKeyboard from '../../utils/custom-keyboard';
 
 /**
  * Generate Wallet
@@ -45,48 +44,4 @@ export const activateTestNetTest = () => {
             await utils.expectElementVisible('testnet-badge');
         });
     });
-};
-
-/**
- * Add Token
- * @param contractAddress
- */
-export const addToken = async (contractAddress: string) => {
-    await utils.tapElementById('dashboard-menu-icon');
-
-    // Manage Account Screen
-    await utils.tapElementById('manage-account');
-    await utils.tapElementById('add-icon');
-
-    // Manage Token Screen
-    await utils.typeTextElementById('search-input', contractAddress);
-    await utils.tapElementByLabel('Find');
-    await utils.tapElementById('found-token');
-    await utils.tapElementByLabel('Save');
-
-    await utils.tapBackButton();
-};
-
-/**
- * Send Token
- * @param amount
- */
-export const sendToken = async (amount: string) => {
-    // Send Screen
-
-    // Select address
-    await utils.tapElementById('transfer-between-accounts');
-    await utils.tapElementById('account-2');
-    await utils.tapElementById('next');
-
-    // Enter amount
-    await utils.typeTextElementById('enter-amount', amount);
-    await utils.tapElementById('next');
-
-    // Confirm Transaction
-    await utils.tapElementById('confirm');
-
-    // Password Pin Screen
-    await utils.expectElementVisible('password-pin-screen');
-    await customKeyboard.typeWord(utils.PIN_CODE_GENERATE_WALLET); // enter pin code
 };

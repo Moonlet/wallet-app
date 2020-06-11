570e431497ec0c7b82109329c8bd949d05da8dcc
Andrei Novac <32679726+NovacAndrei@users.noreply.github.com>
2020-06-11 15:30:56 +0300

    Full address - Receive Screen (#803)
    * full address
    
    * fix test

--- src/screens/receive/styles.ts
+++ src/screens/receive/styles.ts
@@ -1,6 +1,11 @@
 import { StyleSheet } from 'react-native';
 import { ITheme } from '../../core/theme/itheme';
-import { BASE_DIMENSION, SCREEN_HEIGHT } from '../../styles/dimensions';
+import {
+    BASE_DIMENSION,
+    SCREEN_HEIGHT,
+    normalizeFontAndLineHeight,
+    LETTER_SPACING
+} from '../../styles/dimensions';
 
 export default (theme: ITheme) =>
     StyleSheet.create({
@@ -21,10 +26,19 @@ export default (theme: ITheme) =>
         },
         qrCode: {
             padding: BASE_DIMENSION * 2,
-            backgroundColor: '#FFFFFF'
+            backgroundColor: '#FFFFFF',
+            marginBottom: BASE_DIMENSION + BASE_DIMENSION / 2
         },
         bottomButton: {
             marginHorizontal: BASE_DIMENSION * 2,
             marginBottom: BASE_DIMENSION * 3
+        },
+        fullAddress: {
+            fontSize: normalizeFontAndLineHeight(18),
+            lineHeight: normalizeFontAndLineHeight(25),
+            letterSpacing: LETTER_SPACING,
+            color: theme.colors.textTertiary,
+            textAlign: 'center',
+            paddingHorizontal: BASE_DIMENSION * 5
         }
     });

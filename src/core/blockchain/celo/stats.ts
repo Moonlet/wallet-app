c0b1cc2bf1f1bbaacdbe3835a827543c11200654
Burduhos George <george.burduhos@gmail.com>
2020-08-03 21:20:06 +0300

    Update stats.ts

--- src/core/blockchain/celo/stats.ts
+++ src/core/blockchain/celo/stats.ts
@@ -1,10 +1,8 @@
-import { GenericStats, AccountStats, IStatValueType } from '../types/stats';
+import { GenericStats, AccountStats } from '../types/stats';
 import { Client } from './client';
 import { Contracts } from './config';
 import { IAccountState, ITokenState } from '../../../redux/wallets/state';
 import { ApiClient } from '../../utils/api-client/api-client';
-import BigNumber from 'bignumber.js';
-import { Blockchain } from '../types';
 
 export class Stats extends GenericStats<Client> {
     public async getAccountDelegateStats(
@@ -15,38 +13,6 @@ export class Stats extends GenericStats<Client> {
             account,
             this.client.chainId.toString()
         );
-
-        if (data === undefined) {
-            // get balance if
-            let stats = {
-                topStats: [],
-                chartStats: [],
-                secondaryStats: [],
-                totalAmount: new BigNumber(0),
-                widgets: []
-            };
-
-            if (new BigNumber(token.balance?.value).isGreaterThan(0)) {
-                stats = {
-                    ...stats,
-                    chartStats: [
-                        {
-                            title: 'Available',
-                            color: '#FFFFFF',
-                            type: IStatValueType.AMOUNT,
-                            chartDisplay: true,
-                            data: {
-                                value: token.balance?.value,
-                                tokenSymbol: 'CELO',
-                                blockchain: Blockchain.CELO
-                            }
-                        }
-                    ]
-                };
-            }
-
-            return stats;
-        }
 
         return data;
     }

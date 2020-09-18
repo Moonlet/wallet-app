85fb5527963376542fd89f479bc5fc5ff1afef06
Andrei Novac <andrei.novac.96@gmail.com>
2020-09-18 19:27:44 +0300

    Update styles.ts

--- src/screens/validator-screen/styles.ts
+++ src/screens/validator-screen/styles.ts
@@ -19,20 +19,19 @@ export default (theme: ITheme) =>
         },
         topContainer: {
             flexDirection: 'column',
-            marginBottom: BASE_DIMENSION * 3
+            marginBottom: BASE_DIMENSION * 3,
+            alignItems: 'center'
         },
         topText: {
             fontSize: normalizeFontAndLineHeight(15),
             lineHeight: normalizeFontAndLineHeight(20),
-            color: theme.colors.textSecondary,
-            textAlign: 'center'
+            color: theme.colors.textSecondary
         },
         title: {
             fontSize: normalizeFontAndLineHeight(30),
             lineHeight: normalizeFontAndLineHeight(41),
             color: theme.colors.white,
             letterSpacing: LETTER_SPACING,
-            textAlign: 'center',
             marginVertical: BASE_DIMENSION
         },
         token: {

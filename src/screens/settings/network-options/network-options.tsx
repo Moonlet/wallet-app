848a0912ec30454b67005213da795e7481b42f67
Andrei Novac <32679726+NovacAndrei@users.noreply.github.com>
2020-09-21 11:27:03 +0300

    add remote config (#932)

--- src/screens/settings/network-options/network-options.tsx
+++ src/screens/settings/network-options/network-options.tsx
@@ -68,6 +68,12 @@ export class NetworkOptionsComponent extends React.Component<
                 {networksOptions &&
                     Object.keys(networksOptions).map((blockchain: Blockchain, index: number) => {
                         if (
+                            blockchain === Blockchain.NEAR &&
+                            isFeatureActive(RemoteFeature.NEAR) === false
+                        ) {
+                            return <View key={index} />;
+                        }
+                        if (
                             blockchain === Blockchain.COSMOS &&
                             isFeatureActive(RemoteFeature.COSMOS) === false
                         ) {

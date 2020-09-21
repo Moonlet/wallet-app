2448fb016ebce76cf07eb88b320e428871e7ccac
Andrei Novac <32679726+NovacAndrei@users.noreply.github.com>
2020-09-21 11:37:34 +0300

    Update Amount Comp Design (#920)
    * update style
    
    * update validator text

--- src/components/amount/styles.ts
+++ src/components/amount/styles.ts
@@ -3,10 +3,6 @@ import { ITheme } from '../../core/theme/itheme';
 
 export default (theme: ITheme) =>
     StyleSheet.create({
-        container: {
-            flexDirection: 'row',
-            alignItems: 'baseline'
-        },
         smallToken: {
             color: theme.colors.white
         }

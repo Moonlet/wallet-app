8f918dbae8169846d38f5da27470264dbee828cd
Andrei Novac <andrei.novac.96@gmail.com>
2020-10-02 01:00:33 +0300

    Update consts.ts

--- src/core/blockchain/near/consts.ts
+++ src/core/blockchain/near/consts.ts
@@ -1,4 +1,3 @@
-import BigNumber from 'bignumber.js';
 import BN from 'bn.js';
 
 export const NEAR_DEFAULT_FUNC_CALL_GAS = new BN('100000000000000');
@@ -6,8 +5,6 @@ export const NEAR_DEFAULT_FUNC_CALL_GAS = new BN('100000000000000');
 export const CREATE_ACCOUNT_NEAR_FEES = new BN('1000000000000000000000000');
 export const CREATE_ACCOUNT_NEAR_DEPOSIT = new BN('100000000000000000000000');
 
-export const NEAR_CREATE_ACCOUNT_MIN_BALANCE = new BN(
-    new BigNumber(CREATE_ACCOUNT_NEAR_FEES.toString())
-        .plus(new BigNumber(CREATE_ACCOUNT_NEAR_DEPOSIT.toString()))
-        .toFixed()
+export const NEAR_CREATE_ACCOUNT_MIN_BALANCE = CREATE_ACCOUNT_NEAR_FEES.add(
+    CREATE_ACCOUNT_NEAR_DEPOSIT
 );

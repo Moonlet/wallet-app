b8327fe4303c87bd40028a9484c1072173959b36
Burduhos George <george.burduhos@gmail.com>
2020-10-06 12:52:11 +0300

    [ZIL] Add node Reward balance on Claim widgets #1034  [ZIL] Add relevant message to Unstake flow #1026

--- src/core/i18n/translation/locales/en.ts
+++ src/core/i18n/translation/locales/en.ts
@@ -548,7 +548,11 @@ export const translation: ITranslations = {
             stakeSection: 'This section will become available once you start to stake your tokens.',
             operationNotAvailable: 'Operation not available',
             operationNotAvailableMessage:
-                'This operation can not be performed. Please try again next network cycle'
+                'This operation can not be performed. Please try again next network cycle',
+            restakeScreenMessageZil:
+                'The accumulated ZIL rewards will be automatically claimed and deposited into your available balance, as well gZIL.',
+            unstakeScreenMessageZil:
+                'It takes 14 days in order to receive the amount. The accumulated ZIL rewards will be automatically claimed and deposited into your available balance, as well gZIL.'
         },
         Widget: {
             activateVotesTitle: 'Activate your votes now',

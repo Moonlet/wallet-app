028ca06648e6f00a7549c7e82f00ffc2f39af07c
Andrei Novac <andrei.novac.96@gmail.com>
2020-10-07 10:34:35 +0300

    fix format address

--- src/core/utils/format-address.ts
+++ src/core/utils/format-address.ts
@@ -1,19 +1,31 @@
 import { Blockchain } from '../blockchain/types';
 import { getBlockchain } from '../blockchain/blockchain-factory';
 
+const stripAddress = (text: string) => {
+    if (text.length === 0) {
+        return '';
+    }
+    let convertedStr = '';
+    convertedStr += text.substring(0, 5);
+    convertedStr += '.'.repeat(3);
+    convertedStr += text.substring(text.length - 5, text.length);
+    return convertedStr;
+};
+
 export const formatAddress = (text: string, blockchain: Blockchain) => {
     if (!text) return '';
+
+    const split = text.split('.');
+    if (split.length > 1 && split[0].length > 20) {
+        split[0] = stripAddress(split[0]);
+        return split.join('.');
+    }
+
     if (text.match(/.+\.[a-zA-Z]{2,}$/gi)) return text;
+
     switch (getBlockchain(blockchain).config.ui.addressDisplay) {
         case 'stripped':
-            if (text.length === 0) {
-                return '';
-            }
-            let convertedStr = '';
-            convertedStr += text.substring(0, 5);
-            convertedStr += '.'.repeat(3);
-            convertedStr += text.substring(text.length - 5, text.length);
-            return convertedStr;
+            return stripAddress(text);
         default:
             return text;
     }

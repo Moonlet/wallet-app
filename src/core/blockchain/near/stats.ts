2218742485bcf39f3adf0fd33fca818acb98864c
Stoica Andrei <stoica.andrei32@gmail.com>
2020-10-09 00:27:57 +0300

    Fixed typo

--- src/core/blockchain/near/stats.ts
+++ src/core/blockchain/near/stats.ts
@@ -29,7 +29,7 @@ export class Stats extends GenericStats<Client> {
         );
 
         let availableToDelegate = new BigNumber(data.balance.unstaked || 0);
-        if (new BigNumber(data.balace.available).gt(config.amountToKeepInAccount)) {
+        if (new BigNumber(data.balance.available).gt(config.amountToKeepInAccount)) {
             availableToDelegate = availableToDelegate.plus(
                 new BigNumber(data.balance.available).minus(config.amountToKeepInAccount)
             );

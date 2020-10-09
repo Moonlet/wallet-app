c894e5d0604d00fac2eb56f2df3f18806ac991e8
Stoica Andrei <stoica.andrei32@gmail.com>
2020-10-09 16:53:53 +0300

    Fixed QR code style and address style.

--- src/screens/receive/styles.ts
+++ src/screens/receive/styles.ts
@@ -11,8 +11,7 @@ export default (theme: ITheme) =>
     StyleSheet.create({
         container: {
             flex: 1,
-            padding: BASE_DIMENSION * 2,
-            paddingTop: BASE_DIMENSION * 4,
+            // padding: BASE_DIMENSION * 2,
             flexDirection: 'column',
             backgroundColor: theme.colors.appBackground,
             height: SCREEN_HEIGHT // used for web
@@ -28,8 +27,9 @@ export default (theme: ITheme) =>
             marginBottom: BASE_DIMENSION * 2
         },
         qrCode: {
-            padding: BASE_DIMENSION * 2,
-            color: theme.colors.white
+            color: theme.colors.white,
+            borderColor: theme.colors.white,
+            borderWidth: BASE_DIMENSION * 2
         },
         bottomButton: {
             marginHorizontal: BASE_DIMENSION * 2,
@@ -42,8 +42,8 @@ export default (theme: ITheme) =>
         fullAddress: {
             flexShrink: 1,
             flexWrap: 'wrap',
-            fontSize: normalizeFontAndLineHeight(18),
-            lineHeight: normalizeFontAndLineHeight(25),
+            fontSize: normalizeFontAndLineHeight(17),
+            lineHeight: normalizeFontAndLineHeight(20),
             letterSpacing: LETTER_SPACING,
             color: theme.colors.textTertiary,
             textAlign: 'center',

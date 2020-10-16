fd646b39f5da96c5caffded528ae07e9f9928fbc
Andrei Novac <32679726+NovacAndrei@users.noreply.github.com>
2020-10-16 11:30:02 +0300

    Update Validators list style (#1191)
    * update validators style
    
    * add empty state

--- src/screens/pos-actions/pos-action-quick-delegate/quick-delegate-select-validator/styles.ts
+++ src/screens/pos-actions/pos-action-quick-delegate/quick-delegate-select-validator/styles.ts
@@ -1,12 +1,11 @@
-import { StyleSheet, Platform } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { ITheme } from '../../../../core/theme/itheme';
+import { ph, pw } from '../../../../styles';
 import {
     BASE_DIMENSION,
-    BORDER_RADIUS,
-    SCREEN_HEIGHT,
-    normalize,
-    isIphoneXorAbove,
-    normalizeFontAndLineHeight
+    LETTER_SPACING,
+    normalizeFontAndLineHeight,
+    SCREEN_HEIGHT
 } from '../../../../styles/dimensions';
 
 export default (theme: ITheme) =>
@@ -18,50 +17,39 @@ export default (theme: ITheme) =>
             height: SCREEN_HEIGHT // used for web,
         },
         content: {
-            flex: 1,
-            paddingTop: BASE_DIMENSION * 2,
-            paddingBottom: Platform.select({
-                default: BASE_DIMENSION * 10,
-                ios: isIphoneXorAbove() ? BASE_DIMENSION * 11 : BASE_DIMENSION * 10
-            })
+            flex: 1
         },
         listContainer: {
-            marginTop: BASE_DIMENSION * 3,
+            flex: 1,
             marginHorizontal: BASE_DIMENSION * 2
         },
-        actionContainer: {
-            marginTop: BASE_DIMENSION * 2,
-            width: normalize(170),
-            flexDirection: 'row',
-            borderRadius: BORDER_RADIUS / 2,
-            borderColor: theme.colors.accent,
-            borderWidth: 1,
-            justifyContent: 'center',
-            alignSelf: 'center'
-        },
-        actionCounterText: {
-            flex: 1,
-            fontSize: normalizeFontAndLineHeight(17),
-            lineHeight: normalizeFontAndLineHeight(20),
-            fontWeight: '600',
-            color: theme.colors.text,
-            textAlign: 'center',
-            alignSelf: 'center'
-        },
-        actionIconContainer: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            alignContent: 'center',
-            paddingVertical: BASE_DIMENSION / 2,
-            paddingHorizontal: BASE_DIMENSION / 2
-        },
-        actionIcon: {
-            color: theme.colors.accent,
-            alignSelf: 'center'
+        validatorsList: {
+            paddingTop: BASE_DIMENSION * 2,
+            paddingBottom: BASE_DIMENSION * 11
         },
         loadingContainer: {
             flex: 1,
             justifyContent: 'center'
+        },
+        emptySection: {
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center'
+        },
+        logoImage: {
+            height: ph(20),
+            width: pw(50),
+            alignSelf: 'center',
+            resizeMode: 'contain',
+            marginBottom: BASE_DIMENSION * 2
+        },
+        noNodesText: {
+            fontSize: normalizeFontAndLineHeight(22),
+            lineHeight: normalizeFontAndLineHeight(28),
+            fontWeight: 'bold',
+            textAlign: 'center',
+            letterSpacing: LETTER_SPACING,
+            color: theme.colors.textSecondary,
+            marginBottom: BASE_DIMENSION * 2
         }
     });

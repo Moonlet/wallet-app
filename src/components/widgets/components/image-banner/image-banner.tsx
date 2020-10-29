46f3e6233ffa520864e0a994386c0a07aafef801
Andrei Novac <andrei.novac.96@gmail.com>
2020-10-29 19:58:49 +0200

    Update image-banner.tsx

--- src/components/widgets/components/image-banner/image-banner.tsx
+++ src/components/widgets/components/image-banner/image-banner.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
 import { smartConnect } from '../../../../core/utils/smart-connect';
-import { withTheme } from '../../../../core/theme/with-theme';
+import { IThemeProps, withTheme } from '../../../../core/theme/with-theme';
 import stylesProvider from './styles';
 import { openURL } from '../../../../core/utils/linking-handler';
 import { ResizeMode, SmartImage } from '../../../../library/image/smart-image';
@@ -11,15 +11,11 @@ interface IExternalProps {
     urlToOpen: string;
 }
 
-interface Props {
-    styles: ReturnType<typeof stylesProvider>;
-}
+const ImageBannerComponent = (
+    props: IThemeProps<ReturnType<typeof stylesProvider>> & IExternalProps
+) => {
+    const { imageUrl, urlToOpen, styles } = props;
 
-const ImageBannerComponent: React.FC<Props & IExternalProps> = ({
-    imageUrl,
-    urlToOpen,
-    styles
-}) => {
     return (
         <TouchableOpacity onPress={() => openURL(urlToOpen)} activeOpacity={0.9}>
             <SmartImage

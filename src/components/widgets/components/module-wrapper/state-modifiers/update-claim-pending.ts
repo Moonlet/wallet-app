330340db6f3612b8b195e2a0998de6595c807cf0
Andrei Novac <andrei.novac.96@gmail.com>
2020-11-13 17:56:39 +0200

    claimed status

--- src/components/widgets/components/module-wrapper/state-modifiers/update-claim-pending.ts
+++ src/components/widgets/components/module-wrapper/state-modifiers/update-claim-pending.ts
@@ -10,14 +10,21 @@ export const updateClaimPending = (state: IReduxState, module: IScreenModule): s
     const wallet = getSelectedWallet(state);
     const selectedAccount = getSelectedAccount(state);
 
-    for (const tx of Object.values(wallet?.transactions || [])) {
+    const wrapperCycleStart = wrapper?.data?.DEFAULT?.details?.cycleStart;
+    const wrapperValidators = wrapper?.data?.DEFAULT?.details?.validators;
+
+    const walletTransactions = wallet?.transactions || [];
+
+    for (const tx of Object.values(walletTransactions)) {
+        const posAction = tx?.additionalInfo?.posAction;
+
         if (
             tx?.data?.params &&
             tx?.data?.params[0] === wrapper?.data?.DEFAULT?.details?.validatorId &&
-            tx?.broadcastedOnBlock > wrapper?.data?.DEFAULT?.details?.cycleStart &&
+            tx?.broadcastedOnBlock > wrapperCycleStart &&
             tx?.address === selectedAccount.address &&
-            (tx?.additionalInfo?.posAction === PosBasicActionType.CLAIM_REWARD ||
-                tx?.additionalInfo?.posAction === PosBasicActionType.CLAIM_REWARD_NO_INPUT)
+            (posAction === PosBasicActionType.CLAIM_REWARD ||
+                posAction === PosBasicActionType.CLAIM_REWARD_NO_INPUT)
         ) {
             switch (tx.status) {
                 case TransactionStatus.PENDING:
@@ -29,19 +36,44 @@ export const updateClaimPending = (state: IReduxState, module: IScreenModule): s
             }
         }
 
-        // Claim all module
+        // Claim all module - PENDING
         if (
             // at least one claim tx in pending
             tx.status === TransactionStatus.PENDING &&
-            (tx?.additionalInfo?.posAction === PosBasicActionType.CLAIM_REWARD ||
-                tx?.additionalInfo?.posAction === PosBasicActionType.CLAIM_REWARD_NO_INPUT) &&
+            (posAction === PosBasicActionType.CLAIM_REWARD ||
+                posAction === PosBasicActionType.CLAIM_REWARD_NO_INPUT) &&
             // claim all module is identified by validators
-            wrapper?.data?.DEFAULT?.details?.validators &&
+            wrapperValidators &&
             // additional checks
-            tx?.broadcastedOnBlock > wrapper?.data?.DEFAULT?.details?.cycleStart &&
+            tx?.broadcastedOnBlock > wrapperCycleStart &&
             tx?.address === selectedAccount.address
         ) {
             return 'PENDING';
+        }
+    }
+
+    // Claim all module - CLAIMED
+    if (wrapperValidators) {
+        let allClaimed = true;
+        for (const v of wrapperValidators) {
+            const index = Object.values(walletTransactions).findIndex(
+                tx =>
+                    // success claim tx
+                    tx?.data?.params[0] === v.validatorId &&
+                    tx.status === TransactionStatus.SUCCESS &&
+                    (tx?.additionalInfo?.posAction === PosBasicActionType.CLAIM_REWARD ||
+                        tx?.additionalInfo?.posAction ===
+                            PosBasicActionType.CLAIM_REWARD_NO_INPUT) &&
+                    // additional checks
+                    tx?.broadcastedOnBlock > wrapperCycleStart &&
+                    tx?.address === selectedAccount.address
+            );
+            if (index === -1) {
+                allClaimed = false;
+            }
+        }
+        if (allClaimed) {
+            return 'CLAIMED';
         }
     }
 

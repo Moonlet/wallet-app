de13072264244b9219398270540f1a90552223c0
Andrei Novac <andrei.novac.96@gmail.com>
2020-12-01 17:30:44 +0200

    update image banner

--- src/components/widgets/components/image-banner/image-banner.tsx
+++ src/components/widgets/components/image-banner/image-banner.tsx
@@ -1,37 +1,70 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, Image } from 'react-native';
 import { smartConnect } from '../../../../core/utils/smart-connect';
 import { IThemeProps, withTheme } from '../../../../core/theme/with-theme';
 import stylesProvider from './styles';
-import { ResizeMode, SmartImage } from '../../../../library/image/smart-image';
+import { ResizeMode } from '../../../../library/image/smart-image';
 import { IImageBannerData, IScreenModule, ISmartScreenActions } from '../../types';
 import { formatStyles } from '../../utils';
+import FastImage from 'react-native-fast-image';
 
 interface IExternalProps {
     module: IScreenModule;
     actions: ISmartScreenActions;
 }
 
-const ImageBannerComponent = (
-    props: IThemeProps<ReturnType<typeof stylesProvider>> & IExternalProps
-) => {
-    const { actions, module, styles } = props;
-    const data = module.data as IImageBannerData;
+interface IState {
+    aspectRatio: number;
+}
 
-    return (
-        <TouchableOpacity
-            onPress={() => actions.handleCta(module.cta)}
-            activeOpacity={0.9}
-            style={module?.style && formatStyles(module.style)}
-        >
-            <SmartImage
-                style={styles.image}
+class ImageBannerComponent extends React.Component<
+    IThemeProps<ReturnType<typeof stylesProvider>> & IExternalProps,
+    IState
+> {
+    constructor(props: IThemeProps<ReturnType<typeof stylesProvider>> & IExternalProps) {
+        super(props);
+        this.state = {
+            aspectRatio: 1
+        };
+    }
+
+    public componentDidMount() {
+        Image.getSize(
+            (this.props.module.data as IImageBannerData).imageUrl,
+            (width, height) => this.setState({ aspectRatio: width / height }),
+            error => {
+                // no need to handle
+            }
+        );
+    }
+
+    public render() {
+        const { actions, module, styles } = this.props;
+        const data = module.data as IImageBannerData;
+
+        const moduleJSX = (
+            <FastImage
+                style={[
+                    { aspectRatio: this.state.aspectRatio },
+                    styles.image,
+                    formatStyles(module?.style)
+                ]}
                 source={{ uri: data.imageUrl }}
                 resizeMode={ResizeMode.contain}
             />
-        </TouchableOpacity>
-    );
-};
+        );
+
+        if (module?.cta) {
+            return (
+                <TouchableOpacity onPress={() => actions.handleCta(module.cta)} activeOpacity={0.9}>
+                    {moduleJSX}
+                </TouchableOpacity>
+            );
+        } else {
+            return moduleJSX;
+        }
+    }
+}
 
 export const ImageBanner = smartConnect<IExternalProps>(ImageBannerComponent, [
     withTheme(stylesProvider)

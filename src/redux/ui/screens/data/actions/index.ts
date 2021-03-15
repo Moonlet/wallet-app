b6830e74851348e1cd6f64f59862fa401269adb8
Burduhos George <george.burduhos@gmail.com>
2021-03-15 12:53:44 +0200

    Update index.ts

--- src/redux/ui/screens/data/actions/index.ts
+++ src/redux/ui/screens/data/actions/index.ts
@@ -7,18 +7,18 @@ export const supportedActions = flattenObject({
     transactions
 });
 
+export interface IHandleCtaOptions {
+    screenKey?: string;
+    validator?: {
+        id: string;
+        name: string;
+        icon?: string;
+        website?: string;
+    };
+    pubSub?: PubSub<SmartScreenPubSubEvents>;
+}
+
 export interface IHandleCtaActionContext<P = any> {
     action: ICtaAction<P>;
-
-    // TODO = make interface
-    options?: {
-        screenKey?: string;
-        validator?: {
-            id: string;
-            name: string;
-            icon?: string;
-            website?: string;
-        };
-        pubSub?: PubSub<SmartScreenPubSubEvents>;
-    };
+    options?: IHandleCtaOptions;
 }

cfe790c3515d23d4c1665f4fe09f06c00ac69520
Andrei Novac <andrei.novac.96@gmail.com>
2021-04-20 12:27:24 +0300

    update whitelist

--- src/redux/ui/screens/data/actions/transactions/transaction-builder.ts
+++ src/redux/ui/screens/data/actions/transactions/transaction-builder.ts
@@ -18,9 +18,10 @@ import { Contracts } from '../../../../../../core/blockchain/zilliqa/config';
 
 const contractCallFunctionsWhitelist = {
     [Blockchain.ZILLIQA]: [
-        SwapContractMethod.SWAP_EXACT_TOKENS_FOR_ZIL,
+        SwapContractMethod.INCREASE_ALLOWANCE,
         SwapContractMethod.SWAP_EXACT_ZIL_FOR_TOKENS,
-        SwapContractMethod.INCREASE_ALLOWANCE
+        SwapContractMethod.SWAP_EXACT_TOKENS_FOR_ZIL,
+        SwapContractMethod.SWAP_EXACT_TOKENS_FOR_TOKENS
     ]
 };
 

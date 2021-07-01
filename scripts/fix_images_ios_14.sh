16ba0d0278ad40aa3aad919ad826d0ad1867a732
Burduhos George <george.burduhos@gmail.com>
2021-07-01 17:38:51 +0300

    upgrade

--- scripts/fix_images_ios_14.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Fixing Image iOS 14";
-HUYDEV="_currentFrame.CGImage;"
-HUYFIX="_currentFrame.CGImage ;} else { [super displayLayer:layer];"
-sed -ie "s/${HUYDEV}/${HUYFIX}/" node_modules/react-native/Libraries/Image/RCTUIImageViewAnimated.m
-echo "Done."

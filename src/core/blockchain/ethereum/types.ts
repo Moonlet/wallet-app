6f5db27be11964065fa8c562e8c07456a3465a39
Burduhos George <george.burduhos@gmail.com>
2021-07-21 23:06:47 +0300

    increase allowance (#1509)

--- src/core/blockchain/ethereum/types.ts
+++ src/core/blockchain/ethereum/types.ts
@@ -1,5 +1,5 @@
 export enum MethodSignature {
-    INCREASE_ALLOWANCE = 'increaseAllowance(address,uint256)',
+    INCREASE_ALLOWANCE = 'approve(address,uint256)',
     DELEGATE = 'delegate(address,uint256)',
     UNDELEGATE = 'undelegate(address,uint256)',
     WITHDRAW_DELEGATED = 'withdrawDelegated(address,address)',

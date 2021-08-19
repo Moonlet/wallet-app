5b74a6a7670455372c83729696610c1181dbe161
Andrei Novac <andrei.novac.96@gmail.com>
2021-08-19 12:17:58 +0300

    dummy

--- src/screens/token/components/default-token/default-token.tsx
+++ src/screens/token/components/default-token/default-token.tsx
@@ -264,7 +264,7 @@ class DefaultTokenScreenComponent extends React.Component<
                                     testID="send-button"
                                     style={styles.button}
                                     wrapperStyle={{ flex: 1 }}
-                                    // disabled={!this.props.canSend} // TODO OOOOOOOO0
+                                    // disabled={!this.props.canSend} // TODO OOOOOOOO00
                                     onPress={() => {
                                         navigation.navigate('Send', {
                                             accountIndex: account.index,

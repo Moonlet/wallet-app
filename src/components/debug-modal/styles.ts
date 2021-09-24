c870ebfbe8c89b0f0dbceefd1ecedf581cc102b4
Andrei Novac <32679726+NovacAndrei@users.noreply.github.com>
2021-09-24 13:08:24 +0300

    debug modal add solana rewards (#1558)

--- src/components/debug-modal/styles.ts
+++ src/components/debug-modal/styles.ts
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native';
 import { ITheme } from '../../core/theme/itheme';
-import { BASE_DIMENSION } from '../../styles/dimensions';
+import { BASE_DIMENSION, BORDER_RADIUS, normalizeFontAndLineHeight } from '../../styles/dimensions';
 
 export default (theme: ITheme) =>
     StyleSheet.create({
@@ -15,5 +15,28 @@ export default (theme: ITheme) =>
         },
         button: {
             marginVertical: BASE_DIMENSION * 2
+        },
+        inputBox: {
+            height: BASE_DIMENSION * 5,
+            borderRadius: BORDER_RADIUS,
+            alignSelf: 'stretch',
+            backgroundColor: theme.colors.cardBackground,
+            paddingHorizontal: BASE_DIMENSION,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginBottom: BASE_DIMENSION * 2
+        },
+        input: {
+            flex: 1,
+            fontSize: normalizeFontAndLineHeight(15),
+            color: theme.colors.text,
+            padding: 0
+        },
+        epochContainer: {
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginTop: BASE_DIMENSION * 2
         }
     });

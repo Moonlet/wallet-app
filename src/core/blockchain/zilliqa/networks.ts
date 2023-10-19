11d8c5e6263b2aa51fe543cecd6f0a8bc0bcc7ba
novi <andrei.novac.96@gmail.com>
2023-10-19 14:37:51 +0300

    change config beta

--- src/core/blockchain/zilliqa/networks.ts
+++ src/core/blockchain/zilliqa/networks.ts
@@ -24,28 +24,5 @@ export const networks: IBlockchainNetwork[] = [
             getAccountUrl: addr => `https://viewblock.io/zilliqa/address/${addr}?network=testnet`,
             getTransactionUrl: txn => `https://viewblock.io/zilliqa/tx/${txn}?network=testnet`
         }
-    },
-    {
-        name: 'Dev 2',
-        chainId: 333,
-        mainNet: false,
-        url: 'https://api.testnet.zilliqa.com/',
-        wsUrl: 'wss://dev-ws.zilliqa.com/',
-        explorer: {
-            name: 'Viewblock',
-            getAccountUrl: addr => `https://viewblock.io/zilliqa/address/${addr}?network=testnet`,
-            getTransactionUrl: txn => `https://viewblock.io/zilliqa/tx/${txn}?network=testnet`
-        }
-    },
-    {
-        name: 'Kaya Local',
-        chainId: 2,
-        mainNet: false,
-        url: 'http://127.0.0.1:4200/',
-        explorer: {
-            name: 'Viewblock',
-            getAccountUrl: () => ``,
-            getTransactionUrl: () => ``
-        }
     }
 ];

f6e21923ffaa6aeb1744bfa56b701beddebfa9f9
novi <andrei.novac.96@gmail.com>
2023-10-20 17:38:14 +0300

    change config beta

--- src/config/config-beta.ts
+++ src/config/config-beta.ts
@@ -4,7 +4,7 @@ import { firebaseConfig } from './firebase-web-release';
 const extSyncBaseUrl = 'https://fire.moonlet.io/functions/extensionSync';
 
 export const CONFIG_BETA: IAppConfig = {
-    walletApiBaseUrl: 'https://api.moonlet.app',
+    walletApiBaseUrl: 'https://api-preview.moonlet.app',
     supportUrl: 'https://fire.moonlet.io/static/support/webview.html',
     env: process.env.MOONLET_SOME_KEY,
     tokensUrl: 'https://fire.moonlet.io/static/tokens/',
@@ -25,33 +25,3 @@ export const CONFIG_BETA: IAppConfig = {
     },
     firebaseWebConfig: firebaseConfig
 };
-
-// import { IAppConfig } from './config-interface';
-// import { firebaseConfig } from './firebase-web-beta';
-
-// const extSyncBaseUrl = 'https://fire.moonlet.dev/functions/extensionSync';
-
-// export const CONFIG_BETA: IAppConfig = {
-//     walletApiBaseUrl: 'https://api.moonlet.dev',
-//     // walletApiBaseUrl: 'http://127.0.0.1:8080',
-
-//     supportUrl: 'https://fire.moonlet.dev/static/support/webview.html',
-//     env: process.env.MOONLET_SOME_KEY,
-//     tokensUrl: 'https://fire.moonlet.dev/static/tokens/',
-//     termsAndConditionsUrl:
-//         'https://fire.moonlet.dev/static/terms/terms-of-service.html?v=' + Date.now(),
-//     privacyPolicyUrl: 'https://fire.moonlet.dev/static/terms/privacy-policy.html?v=' + Date.now(),
-//     troubleshootingUrl: 'https://fire.moonlet.dev/static/ledger/troubleshooting.html',
-//     firebaseConfigFetchInterval: 0,
-//     ntpServer: 'pool.ntp.org',
-//     ntpPort: 123,
-//     extSync: {
-//         bucket: 'gs://moonlet-extension-sync',
-//         updateStateUrl: extSyncBaseUrl + '/updateState',
-//         disconnectUrl: extSyncBaseUrl + '/disconnect',
-//         sendRequestUrl: extSyncBaseUrl + '/sendRequest',
-//         sendResponseUrl: extSyncBaseUrl + '/sendResponse',
-//         deleteRequestUrl: extSyncBaseUrl + '/deleteRequest'
-//     },
-//     firebaseWebConfig: firebaseConfig
-// };

2c2e420abaa838ea0b771fa646d493985e55af74
novi <andrei.novac.96@gmail.com>
2023-10-23 15:29:51 +0300

    zilliqa change gas limit

--- src/core/blockchain/zilliqa/contracts/staking.ts
+++ src/core/blockchain/zilliqa/contracts/staking.ts
@@ -413,9 +413,7 @@ export class Staking {
 
         // TODO: check this with ZILLIQA team
         let gasLimitBasedOnCycles = 3450; // default value
-        if (nrCyclesPassed >= 1095) {
-            gasLimitBasedOnCycles = 300000;
-        } else if (nrCyclesPassed >= 730) {
+        if (nrCyclesPassed >= 730) {
             gasLimitBasedOnCycles = 200000;
         } else if (nrCyclesPassed >= 365) {
             gasLimitBasedOnCycles = 100000;
